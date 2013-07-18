c77794b7dbf9584e629613bf39ca708a8688bc43
Steffen Mueller <smueller@cpan.org>
2013-07-18 20:25:05 +0200

    Somewhat improved docs

--- lib/PLua.pm
+++ lib/PLua.pm
@@ -54,24 +54,80 @@ PLua - Perl and Lua Make a Great Couple!
 
 =head1 SYNOPSIS
 
+  use 5.14.0;
   use PLua;
   
   my $foo = 12.3;
   lua {
     local bar = $foo.num
     ...
+    bar = bar + 1
     $foo = bar
   }
-  
-  etc.
+  say $foo; # prints 13.3
 
+  my $luatable;
+  lua {{
+    local mytbl = { foo = "bar", inner = {5, 8} }
+    $luatable = mytbl
+  }}
+  
+  say $luatable->get("foo"); # prints "bar"
+  
+  use Data::Dumper;
+  print Dumper $luatable->to_hash(1); # 1 indicates recursive table conversion
+  # $VAR1 = {
+  #         'tbl' => {
+  #                    '1' => '5',
+  #                    '2' => '8'
+  #                  },
+  #         'foo' => "bar",
+  #       };
+  
 =head1 DESCRIPTION
 
-This Perl module aims at providing seamless integration between Perl and Lua.
-At any place in your Perl code, you can now embed blocks of Lua code and from
-within, access your Perl lexicals!
+This Perl module aims at providing seamless integration between Perl and Lua
+and efficiently so. At any place in your Perl code, you can use C<PLua> to embed
+blocks of Lua code and from within, access your Perl lexicals! (But see the
+closure gotcha below.)
 
 TODO write more documentation!
+
+=head1 THE CLOSURE GOTCHA
+
+In a nutshell, Lua functions aren't Perl closures. Maybe this
+can be addressed one day, but for the time being, the author's understanding
+of the implementation of Perl closures isn't strong enough.
+
+Consider the following code:
+
+  if (1) {
+    my $bar = 12;
+    lua {
+      function foo()
+        local tmp = $baz.int + 1
+        $baz = tmp
+        return tmp
+      end
+    }
+  }
+  
+  lua {
+    print foo()
+  }
+
+What do you expect that to print? Clue: The C<foo()> definition
+is within the scope of the Perl lexical, but the call is not.
+C<PLua> performs the lookup of the lexical at compile time, but
+it doesn't have the (quite complex) special magic hot sauce that
+is Perl's closure behaviour.
+
+So in a nutshell, this code may or may not print "13" as you might
+expect. I could also print something else or quite possibly segfault.
+
+So don't use Lua functions to provide access to Perl lexicals outside
+their scope. If you need to do that, send a patch, pay me to work on
+supporting this (just kidding), or work around it. Sorry.
 
 =head1 AUTHOR
 
