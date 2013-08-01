eb099131a3f27b8f9bae43ca3420cd7a37871f9b
Steffen Mueller <smueller@cpan.org>
2013-08-01 07:32:09 +0200

    Keyword parsing cleanup

--- plu_parse_kw.c
+++ plu_parse_kw.c
@@ -11,7 +11,9 @@
 #include <lauxlib.h>
 
 static void
-S_scan_lua_block_delim(pTHX_ const unsigned int ndelimchars, char **outstring, STRLEN *outstringlen)
+S_scan_lua_block_end(pTHX_ const unsigned int ndelimchars,
+                           char **outstring,
+                           STRLEN *outstringlen)
 {
   while (1) {
     char* const end = PL_parser->bufend;
@@ -36,24 +38,21 @@ S_scan_lua_block_delim(pTHX_ const unsigned int ndelimchars, char **outstring, S
   }
 }
 
-static void
-S_parse_lua_block(pTHX_ OP **op_ptr)
+static unsigned int
+S_parse_lua_block_delimiter(pTHX)
 {
+  unsigned int ndelimchars;
   I32 c;
-  unsigned int ndelimchars = 1;
-  char *code_str;
-  STRLEN code_len;
-  int lua_reg_idx;
-  SV *lua_code_sv;
 
   lex_read_space(0);
 
   /* Let's use one or multiple opening curlies as delimiter ... */
   c = lex_read_unichar(0);
   if (c != '{')
-    croak("Can't parse Lua block after 'lua'");
+    croak("Can't find Lua block opening delimiter (one or multiple opening braces)");
+  ndelimchars = 1;
 
-  /* peek first to be able to not eat the first non-delimiter character */
+  /* Peek first to be able to not eat the first non-delimiter character */
   while (1) {
     c = lex_peek_unichar(0);
     if (c != '{')
@@ -62,12 +61,30 @@ S_parse_lua_block(pTHX_ OP **op_ptr)
     ndelimchars++;
   }
 
+  return ndelimchars;
+}
+
+static void
+S_parse_lua_block(pTHX_ OP **op_ptr)
+{
+  unsigned int ndelimchars;
+  char *code_str;
+  STRLEN code_len;
+  int lua_reg_idx;
+  SV *lua_code_sv;
+
+  /* Count {'s */
+  ndelimchars = S_parse_lua_block_delimiter(aTHX);
+
   lex_read_space(0);
 
-  S_scan_lua_block_delim(aTHX_ ndelimchars, &code_str, &code_len);
+  /* Scan to end of matching } x ndelimchars */
+  S_scan_lua_block_end(aTHX_ ndelimchars, &code_str, &code_len);
 
   if (code_str == NULL)
-    croak("Syntax error: cannot find Lua block delimiter");
+    croak("Syntax error: cannot find Lua "
+          "block delimiter of %i closing braces", (int)ndelimchars);
+
   lua_code_sv = sv_2mortal(newSVpvn(code_str, code_len));
   plu_munge_lua_code(aTHX_ lua_code_sv);
   code_str = SvPV(lua_code_sv, code_len);
@@ -86,12 +103,22 @@ int
 plu_my_keyword_plugin(pTHX_ char *keyword_ptr, STRLEN keyword_len, OP **op_ptr) {
   int ret;
 
-  if (keyword_len == 3 && memcmp(keyword_ptr, "lua", 3) == 0) {
+  if (keyword_len == 3 && memcmp(keyword_ptr, "lua", 3) == 0)
+  {
     SAVETMPS;
     S_parse_lua_block(aTHX_ op_ptr);
     ret = KEYWORD_PLUGIN_STMT;
     FREETMPS;
-  } else {
+  }
+  /*else if ( keyword_len == 12
+            && memcmp(keyword_ptr, "lua_function", 12) == 0 )
+  {
+    SAVETMPS;
+    S_parse_lua_block(aTHX_ op_ptr);
+    ret = KEYWORD_PLUGIN_STMT;
+    FREETMPS;
+  }*/
+  else {
     ret = (*PLU_next_keyword_plugin)(aTHX_ keyword_ptr, keyword_len, op_ptr);
   }
 
