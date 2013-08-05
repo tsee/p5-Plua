aa2d9a4cc062be26d8b95ca4dd0d319656589dac
Steffen Mueller <smueller@cpan.org>
2013-08-05 19:41:12 +0200

    Improved documentation

--- lib/PLua.pm
+++ lib/PLua.pm
@@ -76,7 +76,7 @@ PLua - Perl and Lua Make a Great Couple!
     return a + b * c
   }
   say fnord(12, 2, 3); # prints 18
-
+  
   # Pass in or return composite data structures: PLua does the
   # marshalling for you!
   lua_function sum (ary) {
@@ -107,7 +107,7 @@ PLua - Perl and Lua Make a Great Couple!
   }}
   
   say $luatable->get("foo"); # prints "bar"
-
+  
   use PLua qw(RECURSIVE); # get RECURSIVE symbol
   use Data::Dumper;
   print Dumper $luatable->to_hash(RECURSIVE);
@@ -126,7 +126,7 @@ PLua - Perl and Lua Make a Great Couple!
   # But often, you want this:
   $array = $luatable->get("inner")->to_array_shifted();
   # $array = [5, 8];
-
+  
   # You can create Lua tables from Perl explicitly!
   my $tbl = PLua::Table->new;
   $tbl->set_str("foo" => "bar");
@@ -147,9 +147,157 @@ and efficiently so. At any place in your Perl code, you can use C<PLua> to embed
 blocks of Lua code and from within, access your Perl lexicals! (But see the
 closure gotcha below.)
 
-TODO write more documentation!
+On Lua tables, see also the documentation of their OO interface at L<PLua::Table>.
 
-On Lua tables, see also: L<PLua::Table>.
+=head2 Embedding Lua in Perl
+
+As of this writing, C<PLua> has two main avenues for meshing Lua and Perl.
+The first is using the C<lua_function> keyword which gives an
+effective way to embed Lua I<functions> into Perl and exposing those functions
+to Perl. To wit, the example from the synopsis:
+
+  lua_function sum(ary) {
+    -- This is Lua!
+    local s = 0
+    for i, num in ipairs(ary) do
+      s = s + num
+    end
+    return s
+  }
+  
+  # This is Perl!
+  my $sum = sum([1..100]);
+
+To Lua people, this might look a bit odd because Lua doesn't use curly braces
+to delimit code blocks. Therefore, B<before C<PLua> is considered
+production-ready, the syntax may change to use Lua's C<end> instead.>
+The reason why that's not the case right now is that it would require a
+full-blown lua parser to find the closing delimiter. Which brings us to
+delimiters. The scanning for the end of the Lua code is really rather naive
+now, so if your Lua code has any closing curly braces, you need to use more
+curlies as delimiter:
+
+  # THIS IS BROKEN
+  lua_function make_table_broken() {
+    local tbl = {} -- Lua will "end" here
+  }
+
+  # This is okay:
+  lua_function make_table() {{
+    local tbl = {}
+  }}
+  my $tbl = make_table();
+
+You can use an arbitrary number of opening C<{> here.
+
+The second way of interfacing between the two languages is inline Lua code
+blocks:
+
+  lua {
+    local foo = 1
+    for i = 1, 100, 1 do
+      foo = foo + 1
+    end
+    print(foo)
+  }
+
+This will be compiled at Perl compile-time and inserted into the Perl
+OP-tree as a custom OP for maximum run-time efficiency.
+But it doesn't interact with Perl at all just yet. Thus, you can access
+lexical (scalar) Perl variables directly in your Lua code as follows:
+
+  my $n = 100;
+  my $foo;
+  lua {
+    local foo = 1
+    for i = 1, $n.int, 1 do
+      foo = foo + 1
+    end
+    $foo = foo
+  }
+  say "foo is $foo (which should be " . ($n+1) . ")"
+
+See how it's used both for reading and writing? For reading from a Perl
+lexical, you use the C<.foo> notation where C<foo> is whatever type you
+want to cast the Perl scalar as. More on that below.
+On writing back to a Perl variable, C<PLua> will intuit the necessary
+conversion for you. B<Warning: Due to the way that this is implemented
+at the moment, the expression after C<$foo = > must not span more than
+the currentl line. This will hopefully be addressed before a production-ready
+release of PLua.>
+
+The biggest danger of using the embedded-Perl-lexicals syntax is that
+you B<must not use them in Lua functions>. This means that:
+
+  my $x = 2;
+  lua {
+    function square(a)
+      return a * a
+    end
+    $x = square($x.int)
+  }
+
+is just fine, but this is not:
+
+  my $x = 2;
+  lua {
+    function square()
+      -- Really BAD! Never use Perl lexicals in Lua functions
+      $x = $x.int * $x.int
+    end
+  }
+
+Why? Because those Lua function can be called from outside the scope of
+the Perl lexical. See L</"THE CLOSURE GOTCHA"> for details.
+
+=head2 Marshalling Data Between Lua and Perl
+
+The general rule on migrating data between Lua and Perl is
+that the language on the receiving end of
+the conversion determines how things are converted.
+
+For reading from Perl scalars in Lua blocks, you use the C<$foo.XXX>
+syntax, where C<XXX> can be any of:
+
+=over 2
+
+=item I<int>
+
+=item I<num>
+
+=item I<str>
+
+=item I<table>
+
+=item I<any>
+
+=back
+
+C<int>, C<num>, C<str> convert to the respective basic
+types (integers, floats, strings). C<table> requires the Perl
+variable to hold a C<Plua::Table> object (see below). C<any>
+in turn is the wildcard: It will attempt to intuit the type
+of data in the Perl variable and convert accordingly.
+
+C<any> supports integers, floats, strings, C<Plua::Table> objects,
+array references, and hash references. The latter three types
+can be arbitrarily nested and will all be converted to Lua
+tables on the Lua side.
+
+C<lua_function> arguments are converted with the C<any> conversion
+logic, so all of the above are supported.
+
+Lexical Perl variable assignments in Lua blocks also dynamically
+support converting integers, floats, strings, and tables to Perl
+data structures. The Lua tables turn into C<PLua::Table> objects.
+These can then be (shallowly or recursively) converted to
+Perl arrays or hashes, or be inspected manually. This intermediary
+is necessary because Lua tables simply don't universally map to
+any singe Perl type. They are a bit of a bastard child of a dictionary
+and an array.
+
+The same conversion happens to Lua values returned from C<lua_function>s
+to Perl.
 
 =head1 THE CLOSURE GOTCHA
 
@@ -192,7 +340,7 @@ cases.
 
 =head1 ACKNOWLEDGMENTS
 
-Code and ideas from a number of other modules has gone into PLua.
+Code and ideas from a number of other modules has gone into C<PLua>.
 Several people have graciously given advice and support. Apologies
 if this is not a complete list.
 
@@ -210,7 +358,7 @@ Rob Hoelz' advice on Lua as a language and in terms of embedding
 has been invaluable and prevented falling into a number of very
 spiky traps.
 
-Last but not least, Dave Mitchell took the time to write up such a clear
+Last but certainly not least, Dave Mitchell took the time to write up such a clear
 explanation of how lexicals and closures are implemented in perl that
 even I could follow. His ability to convey very intricate technical
 concepts is really quite humbling.
